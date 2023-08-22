38507798ce7c487ebb3fd278d9b0d63f599d1c0e
melchibanza <melkisbanza6@gmail.com>
2023-08-22 21:25:02 +0100

    Creation tabeleau avec explication des marques

--- index.js
+++ index.js
@@ -20,3 +20,26 @@ const marquesBeaute = [
     "Tarte Cosmetics",
     "Laura Mercier"
   ];
+
+  const explicationsMarques = [
+    "L'Oréal - Une marque de cosmétiques française offrant une large gamme de produits de maquillage, de soins capillaires et de soins de la peau.",
+    "Maybelline - Une marque populaire de produits cosmétiques proposant des mascaras, des fonds de teint, des rouges à lèvres et bien plus encore.",
+    "MAC Cosmetics - Une marque de maquillage professionnelle réputée pour ses produits haut de gamme et ses collaborations avec des artistes renommés.",
+    "NARS - Une marque de cosmétiques offrant des produits de maquillage de qualité, notamment des fonds de teint, des blushs et des rouges à lèvres.",
+    "Estée Lauder - Une marque de luxe proposant des produits de soins de la peau, des parfums et du maquillage de haute qualité.",
+    "Chanel - Une marque de luxe proposant une large gamme de produits de beauté, notamment des parfums, des produits de soins de la peau et du maquillage.",
+    "Dior - Une marque de mode et de beauté offrant des produits de maquillage, des parfums et des soins de la peau de luxe.",
+    "Urban Decay - Une marque de cosmétiques connue pour ses palettes de fards à paupières et ses produits de maquillage audacieux.",
+    "Too Faced - Une marque de cosmétiques proposant des produits de maquillage colorés et ludiques, notamment des palettes de fards à paupières et des rouges à lèvres.",
+    "Benefit - Une marque de cosmétiques proposant une large gamme de produits pour le visage, notamment des mascaras, des fards à joues et des enlumineurs.",
+    "Clinique - Une marque de soins de la peau et de maquillage réputée pour ses produits doux et adaptés aux peaux sensibles.",
+    "Guerlain - Une marque de luxe proposant des parfums, des produits de maquillage et des soins de la peau de haute qualité.",
+    "Yves Saint Laurent - Une marque de mode et de beauté offrant des produits de maquillage, des parfums et des soins de la peau élégants.",
+    "Bobbi Brown - Une marque de maquillage réputée pour ses produits naturels et ses fonds de teint adaptés à toutes les carnations.",
+    "Anastasia Beverly Hills - Une marque de cosmétiques spécialisée dans les produits pour les sourcils, les palettes de fards à paupières et les produits de contouring.",
+    "Huda Beauty - Une marque de cosmétiques créée par la célèbre blogueuse Huda Kattan, proposant des produits de maquillage polyvalents et hautement pigmentés.",
+    "Fenty Beauty - Une marque de cosmétiques créée par la chanteuse Rihanna, offrant une large gamme de produits de maquillage adaptés à tous les tons de peau.",
+    "Kylie Cosmetics - Une marque de cosmétiques créée par Kylie Jenner, proposant des produits de maquillage tendance, notamment des rouges à lèvres liquides et des palettes de fards à paupières.",
+    "Tarte Cosmetics - Une marque de cosmétiques proposant des produits naturels et respectueux de l'environnement, notamment des fonds de teint et des palettes de fards à paupières.",
+    "Laura Mercier - Une marque de maquillage réputée pour ses produits de qualité, notamment ses poudres fixatrices et ses produits pour le teint."
+  ];
