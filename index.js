8359a8255c07344964bead0857ff499e4eb27e84
melchibanza <melkisbanza6@gmail.com>
2023-08-22 21:23:52 +0100

    Creation tabeleau de marque de beauté

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const salutation = ['Bonjour', 'Salut', 'Hello', 'Hi'];
+const marquesBeaute = [
+    "L'Oréal",
+    "Maybelline",
+    "MAC Cosmetics",
+    "NARS",
+    "Estée Lauder",
+    "Chanel",
+    "Dior",
+    "Urban Decay",
+    "Too Faced",
+    "Benefit",
+    "Clinique",
+    "Guerlain",
+    "Yves Saint Laurent",
+    "Bobbi Brown",
+    "Anastasia Beverly Hills",
+    "Huda Beauty",
+    "Fenty Beauty",
+    "Kylie Cosmetics",
+    "Tarte Cosmetics",
+    "Laura Mercier"
+  ];
